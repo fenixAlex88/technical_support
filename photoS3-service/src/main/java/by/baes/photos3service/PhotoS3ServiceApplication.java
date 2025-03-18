5f852675e6e0bdcbcd6f7aaba6f6493dc0453a50
Alex Pekar <fenix_al@tut.by>
2025-03-18 09:28:45 +0300

    add photoS3service

---
+++ photoS3-service/src/main/java/by/baes/photos3service/PhotoS3ServiceApplication.java
@@ -0,0 +1,13 @@
+package by.baes.photos3service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoS3ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoS3ServiceApplication.class, args);
+    }
+
+}
---
+++ photoS3-service/src/test/java/by/baes/photos3service/PhotoS3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.baes.photos3service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoS3ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
